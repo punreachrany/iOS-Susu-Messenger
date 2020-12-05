d4e9faab959bca212756ea3366caef789e430da9
Punreach Rany <punreachrany@Punreachs-MacBook-Pro.local>
2020-12-05 21:45:31 +0900

    first commit

---
+++ iOS-Susu-Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Susu-Messenger
+//
+//  Created by Punreach Rany on 2020/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
